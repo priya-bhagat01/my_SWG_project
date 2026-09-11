43d6e1fe6eb2fc0f73e93c2ba2d659d0063b532e
priya-bhagat01 <pb337205@gmail.com>
2026-09-11 14:01:49 +0530

    Generates computer random move and player move

---
+++ main.py
@@ -0,0 +1,20 @@
+#Take user input, Make computer random move 
+import random
+
+computerMove = random.choice([-1, 0, 1])
+'''
+-1 = Water
+0 = Gun
+1 = Snake
+'''
+playerInput = (input("Enter your move from swg: "))
+playerDict = {
+    "s": 1 ,
+    "g": 0 ,
+    "w": -1
+}
+inputDict = {
+    1: "Snake" ,
+    0: "Gun" ,
+    -1: "Water"
+}
